25c9471efb052111646954b34493c72f400bbd74
PhilBroderickCrezco <phil.broderick@crezco.com>
2025-05-15 15:36:32 +0100

    Upload/download nuget packages

--- .buildkite/scripts/restore.sh
+++ .buildkite/scripts/restore.sh
@@ -18,20 +18,28 @@ EOF
     FILEPATH=$(echo "$project" | jq -r '.FilePath')
     
     cat <<EOF >> dynamic-steps.yml
-    - label: ":package: Restore $NAME"
+    - label: ":dotnet: Restore $NAME"
       key: "restore-$SANITIZED_NAME"
       command: |
         dotnet restore "$FILEPATH" --packages ".nuget/${NAME}/packages"
       plugins:
         - docker:
             image: mcr.microsoft.com/dotnet/sdk:9.0
-    - label: ":package: Build $NAME"
+        - artifacts:
+            compressed: ".nuget/${NAME}/packages.tgz"
+            upload: ".nuget/${NAME}/packages"
+    
+    - label: ":dotnet: Build $NAME"
       depends_on: "restore-$SANITIZED_NAME"
       command: |
         dotnet build --configuration Release --no-restore  "$FILEPATH"
       plugins:
+        - artifacts:
+            download: ".nuget/${NAME}/packages"
+            compressed: ".nuget/${NAME}/packages.tgz"
         - docker:
             image: mcr.microsoft.com/dotnet/sdk:9.0
+        
 EOF
   done
   cat dynamic-steps.yml
