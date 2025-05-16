fa1210169d749d04b6abd3ffae6504b1ea4cbf13
PhilBroderickCrezco <phil.broderick@crezco.com>
2025-05-16 15:43:34 +0100

    Uncomment pipeline upload

--- .buildkite/scripts/restore.sh
+++ .buildkite/scripts/restore.sh
@@ -50,7 +50,7 @@ EOF
   cat dynamic-steps.yml
   
   #Upload the dynamic steps to Buildkite
-  #buildkite-agent pipeline upload dynamic-steps.yml
+  buildkite-agent pipeline upload dynamic-steps.yml
 else
   echo "No projects to restore."
 fi
