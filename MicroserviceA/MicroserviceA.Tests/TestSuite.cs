89f40028cf05449ec151ac617d593e52368e2080
PhilBroderickCrezco <phil.broderick@crezco.com>
2025-05-20 17:29:04 +0100

    Remove failing tests

--- MicroserviceA/MicroserviceA.Tests/TestSuite.cs
+++ MicroserviceA/MicroserviceA.Tests/TestSuite.cs
@@ -10,17 +10,17 @@ public class TestSuite
         Assert.True(true);
     }
 
-    [Fact]
-    public void FlakyTest()
-    {
-        // Fail randomly ~50% of the time
-        var chance = Random.NextDouble();
-        Assert.True(chance > 0.5, $"Flaky failure triggered! Random value: {chance}");
-    }
-
-    [Fact]
-    public void AlwaysFails()
-    {
-        Assert.Fail("This test always fails");
-    }
+    // [Fact]
+    // public void FlakyTest()
+    // {
+    //     // Fail randomly ~50% of the time
+    //     var chance = Random.NextDouble();
+    //     Assert.True(chance > 0.5, $"Flaky failure triggered! Random value: {chance}");
+    // }
+    //
+    // [Fact]
+    // public void AlwaysFails()
+    // {
+    //     Assert.Fail("This test always fails");
+    // }
 }
