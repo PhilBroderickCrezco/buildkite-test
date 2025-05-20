730ba1e8e0d632a2a7dba7bbcfce05807ddc4c17
PhilBroderickCrezco <phil.broderick@crezco.com>
2025-05-20 11:30:27 +0100

    Create flakey test

---
+++ MicroserviceA/MicroserviceA.Tests/TestSuite.cs
@@ -0,0 +1,26 @@
+﻿namespace MicroserviceA.Tests;
+
+public class TestSuite
+{
+    private static readonly Random Random = new();
+
+    [Fact]
+    public void AlwaysPasses()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void FlakyTest()
+    {
+        // Fail randomly ~50% of the time
+        var chance = Random.NextDouble();
+        Assert.True(chance > 0.5, $"Flaky failure triggered! Random value: {chance}");
+    }
+
+    [Fact]
+    public void AlwaysFails()
+    {
+        Assert.Fail("This test always fails");
+    }
+}
